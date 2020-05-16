36ac29f8b077dbfe95ad71ecc395c3e2ee916899
Diogo Carolo <diogocarolo@gmail.com>
2020-05-16 13:21:34 -0300

    remove pdo

--- index-admin.controller.php
+++ index-admin.controller.php
@@ -23,21 +23,27 @@
 
 	function deletarNoticia($id){
 
-		$query =  $GLOBALS['db']->prepare("DELETE FROM noticias WHERE id = :id");
-		$query->bindValue(':id',$id);
-		$query->execute(); 
-
+		$result = mysql_query("DELETE FROM noticias WHERE id =".$id);
+        if (!$result) {
+            echo 'Could not run query: ' . mysql_error();
+            exit;
+        }
+		
 		getNoticias();
 		header('Location: index-admin.php');
 	}
 
 	function getNoticias(){
 
-		$query =  $GLOBALS['db']->prepare("SELECT* FROM noticias");
-		$query->execute(); 
+		$result = mysql_query("SELECT* FROM noticias");
+        if (!$result) {
+            echo 'Could not run query: ' . mysql_error();
+            exit;
+        }
+
 		$dados = [];
 		
-		while ($row = $query->fetchObject()) {
+		while ($row = mysql_fetch_object($result)) {
 			array_push($dados,$row);
 		}
 		return $dados;
@@ -48,7 +54,7 @@
 		$target_file = $target_dir . basename($files["imagem"]["name"]);
 		$uploadOk = 1;
 		if(isset($_POST["submit"])) {
-			echo "File is an image - " . $check["mime"] . ".";
+			
 			if (move_uploaded_file($files["imagem"]["tmp_name"], $target_file)) {
 				echo "The file ". basename( $files["imagem"]["name"]). " has been uploaded.";
 				$dados['imagem'] = $files['imagem']['name'];
@@ -63,11 +69,14 @@
 	}
 	function criarNoticia($dados){
 
-		$query =  $GLOBALS['db']->prepare("INSERT INTO noticias (titulo,texto,imagem) VALUES (:titulo,:texto,:imagem)");
-		$query->bindValue(':titulo',$dados['titulo']);
-		$query->bindValue(':texto',$dados['texto']);
-		$query->bindValue(':imagem',$dados['imagem']);
-		$query->execute(); 
+		echo("INSERT INTO noticias (titulo,texto,imagem) VALUES (".$dados['titulo'].",".$dados['texto'].",'".$dados['imagem']."')");
+		$result = mysql_query("INSERT INTO noticias (titulo,texto,imagem) VALUES ('".$dados['titulo']."','".$dados['texto']."','".$dados['imagem']."')");
+
+		if (!$result) {
+			echo 'Could not run query: ' . mysql_error();
+			exit;
+		}
+
 
 		header('Location: index-admin.php?criado=true');
 	}
