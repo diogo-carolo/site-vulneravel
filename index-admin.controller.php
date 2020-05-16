5b350194204da4367e90399b889cbd4787341ad6
Diogo Tavares <diogo.tavares@tvglobo.com.br>
2020-05-16 13:03:08 -0300

    error upload

--- index-admin.controller.php
+++ index-admin.controller.php
@@ -22,67 +22,51 @@
 
 
 	function deletarNoticia($id){
-		$result = mysql_query("DELETE FROM noticias WHERE id = " . $id);
 
-		if (!$result) {
-			echo 'Could not run query: ' . mysql_error();
-			exit;
-		}
+		$query =  $GLOBALS['db']->prepare("DELETE FROM noticias WHERE id = :id");
+		$query->bindValue(':id',$id);
+		$query->execute(); 
 
 		getNoticias();
 		header('Location: index-admin.php');
 	}
 
-  function getNoticias(){
+	function getNoticias(){
 
-    $result = mysql_query("SELECT * FROM noticias");
-
-    if (!$result) {
-        echo 'Could not run query: ' . mysql_error();
-        exit;
-    }
-
-    $dados = [];
-    
-    while ($row = mysql_fetch_object($result)) {
-        array_push($dados,$row);
-    }
-
-    return $dados;
+		$query =  $GLOBALS['db']->prepare("SELECT* FROM noticias");
+		$query->execute(); 
+		$dados = [];
+		
+		while ($row = $query->fetchObject()) {
+			array_push($dados,$row);
+		}
+		return $dados;
 	}
 	
 	function criarImagem($dados,$files){
 		$target_dir = "img/";
 		$target_file = $target_dir . basename($files["imagem"]["name"]);
 		$uploadOk = 1;
-		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
-				$check = getimagesize($files["imagem"]["tmp_name"]);
-				// if($check !== false) {
-						echo "File is an image - " . $check["mime"] . ".";
-						if (move_uploaded_file($files["imagem"]["tmp_name"], $target_file)) {
-							echo "The file ". basename( $files["imagem"]["name"]). " has been uploaded.";
-							$dados['imagem'] = $files['imagem']['name'];
-							criarNoticia($dados);	
-						} else {
-								echo "Sorry, there was an error uploading your file.";
-						}
-						$uploadOk = 1;
-				// } else {
-				// 		echo "File is not an image.";
-				// 		$uploadOk = 0;
-				// }
+			$check = getimagesize($files["imagem"]["tmp_name"]);
+			echo "File is an image - " . $check["mime"] . ".";
+			if (move_uploaded_file($files["imagem"]["tmp_name"], $target_file)) {
+				echo "The file ". basename( $files["imagem"]["name"]). " has been uploaded.";
+				$dados['imagem'] = $files['imagem']['name'];
+				criarNoticia($dados);	
+			} else {
+					echo "Sorry, there was an error uploading your file.";
+			}
+			$uploadOk = 1;
 		}
 	}
 	function criarNoticia($dados){
-		$result = mysql_query("INSERT INTO noticias (titulo,texto,imagem) VALUES ('".$dados['texto']."','".$dados['texto']."','".$dados['imagem']."')");
 
-
-		if (!$result) {
-				echo 'Could not run query: ' . mysql_error();
-				exit;
-		}
+		$query =  $GLOBALS['db']->prepare("INSERT INTO noticias (titulo,texto,imagem) VALUES (:titulo,:texto,:imagem)");
+		$query->bindValue(':titulo',$dados['titulo']);
+		$query->bindValue(':texto',$dados['texto']);
+		$query->bindValue(':imagem',$dados['imagem']);
+		$query->execute(); 
 
 		header('Location: index-admin.php?criado=true');
 	}
